9ce6c14d1115d44a29ae7ef23bcbacb69cc226cf
Brody Larson <larsonbm@colostate.edu>
2025-10-05 13:18:06 -0600

    chore: remove unused code

--- src/graphics/renderer.rs
+++ src/graphics/renderer.rs
@@ -1,16 +1,13 @@
 use super::pipeline::RenderPipeline;
 use super::{Color, Geometry, GeometryBuilder, GraphicsContext, Vertex};
 use crate::graphics::transform::Transform;
-use crate::graphics::uniform::{TransformUniform, UniformBuffer};
+use crate::graphics::uniform::{TransformUniform, UniformPool};
 use glam::Mat4;
-use std::io::pipe;
 use std::iter;
-use std::num::NonZeroU64;
 use std::sync::Arc;
 use wgpu::util::DeviceExt;
-use wgpu::wgc::command::DrawKind::Draw;
 use wgpu::{
-    BindGroupLayout, CommandEncoder, LoadOp, RenderPassColorAttachment, RenderPassDescriptor,
+    CommandEncoder, LoadOp, RenderPassColorAttachment, RenderPassDescriptor,
     StoreOp, SurfaceTexture, TextureView,
 };
 use winit::dpi::PhysicalSize;
@@ -75,85 +72,7 @@ impl Renderer {
     }
 }
 
-#[derive(Debug)]
-pub struct UniformPool {
-    buffers: Vec<wgpu::Buffer>,
-    bind_groups: Vec<wgpu::BindGroup>,
-    current_offset: usize,
-    alignment: usize,
-    buffer_size: usize,
-    current_buffer: usize,
-}
 
-impl UniformPool {
-    pub fn new(device: &wgpu::Device, bind_group_layout: &wgpu::BindGroupLayout) -> Self {
-        let alignment = device.limits().min_storage_buffer_offset_alignment as usize;
-        let uniform_size = std::mem::size_of::<TransformUniform>();
-        let aligned_size = ((uniform_size + alignment - 1) / alignment) * alignment;
-
-        const NUM_BUFFERS: usize = 3;
-        const UNIFORMS_PER_BUFFER: usize = 1000;
-
-        let buffer_size = aligned_size * UNIFORMS_PER_BUFFER;
-        let mut buffers = Vec::with_capacity(NUM_BUFFERS);
-        let mut bind_groups = Vec::with_capacity(NUM_BUFFERS);
-
-        for i in 0..NUM_BUFFERS {
-            let buffer = device.create_buffer(&wgpu::BufferDescriptor {
-                label: Some(&format!("Uniform Buffer {i}")),
-                size: buffer_size as wgpu::BufferAddress,
-                usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
-                mapped_at_creation: false,
-            });
-
-            let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-                label: Some(&format!("Bind Group {i}")),
-                layout: bind_group_layout,
-                entries: &[wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding {
-                        buffer: &buffer,
-                        offset: 0,
-                        size: Some(NonZeroU64::new(aligned_size as u64).unwrap()),
-                    }),
-                }],
-            });
-            buffers.push(buffer);
-            bind_groups.push(bind_group);
-        }
-
-        Self {
-            buffers,
-            bind_groups,
-            current_offset: 0,
-            alignment: aligned_size,
-            buffer_size,
-            current_buffer: 0,
-        }
-    }
-
-    pub fn allocate(&mut self, queue: &wgpu::Queue, uniform: &TransformUniform) -> (usize, u32) {
-        if self.current_offset + self.alignment > self.buffer_size {
-            panic!("Buffer Overflow");
-        }
-
-        let offset = self.current_offset;
-        queue.write_buffer(
-            &self.buffers[self.current_buffer],
-            offset as u64,
-            bytemuck::cast_slice(&[*uniform]),
-        );
-
-        let result = (self.current_buffer, offset as u32);
-        self.current_offset += self.alignment;
-        result
-    }
-
-    pub fn next_frame(&mut self) {
-        self.current_buffer = (self.current_buffer + 1) % self.buffers.len();
-        self.current_offset = 0;
-    }
-}
 
 #[derive(Debug, Clone)]
 pub struct DrawCommand {
--- src/graphics/uniform.rs
+++ src/graphics/uniform.rs
@@ -1,3 +1,4 @@
+use std::num::NonZeroU64;
 use bytemuck::{Pod, Zeroable};
 use glam::Mat4;
 use wgpu::util::DeviceExt;
@@ -18,39 +19,82 @@ impl TransformUniform {
     }
 }
 
-pub struct UniformBuffer {
-    pub buffer: wgpu::Buffer,
-    pub bind_group: wgpu::BindGroup,
+#[derive(Debug)]
+pub struct UniformPool {
+    pub buffers: Vec<wgpu::Buffer>,
+    pub bind_groups: Vec<wgpu::BindGroup>,
+    pub current_offset: usize,
+    pub alignment: usize,
+    pub buffer_size: usize,
+    pub current_buffer: usize,
 }
 
-impl UniformBuffer {
+impl UniformPool {
     pub fn new(device: &wgpu::Device, bind_group_layout: &wgpu::BindGroupLayout) -> Self {
-        let buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Transform Uniform Buffer"),
-            contents: bytemuck::cast_slice(&[TransformUniform::new(
-                Mat4::IDENTITY,
-                Mat4::IDENTITY,
-            )]),
-            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
-        });
+        let alignment = device.limits().min_storage_buffer_offset_alignment as usize;
+        let uniform_size = std::mem::size_of::<TransformUniform>();
+        let aligned_size = ((uniform_size + alignment - 1) / alignment) * alignment;
 
-        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            label: Some("Transform Bind Group"),
-            layout: bind_group_layout,
-            entries: &[wgpu::BindGroupEntry {
-                binding: 0,
-                resource: buffer.as_entire_binding(),
-            }],
-        });
+        const NUM_BUFFERS: usize = 3;
+        const UNIFORMS_PER_BUFFER: usize = 1000;
 
-        Self { buffer, bind_group }
+        let buffer_size = aligned_size * UNIFORMS_PER_BUFFER;
+        let mut buffers = Vec::with_capacity(NUM_BUFFERS);
+        let mut bind_groups = Vec::with_capacity(NUM_BUFFERS);
+
+        for i in 0..NUM_BUFFERS {
+            let buffer = device.create_buffer(&wgpu::BufferDescriptor {
+                label: Some(&format!("Uniform Buffer {i}")),
+                size: buffer_size as wgpu::BufferAddress,
+                usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+                mapped_at_creation: false,
+            });
+
+            let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+                label: Some(&format!("Bind Group {i}")),
+                layout: bind_group_layout,
+                entries: &[wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding {
+                        buffer: &buffer,
+                        offset: 0,
+                        size: Some(NonZeroU64::new(aligned_size as u64).unwrap()),
+                    }),
+                }],
+            });
+            buffers.push(buffer);
+            bind_groups.push(bind_group);
+        }
+
+        Self {
+            buffers,
+            bind_groups,
+            current_offset: 0,
+            alignment: aligned_size,
+            buffer_size,
+            current_buffer: 0,
+        }
     }
 
-    pub fn update(&self, queue: &wgpu::Queue, matrix: Mat4, projection: Mat4) {
+    pub fn allocate(&mut self, queue: &wgpu::Queue, uniform: &TransformUniform) -> (usize, u32) {
+        if self.current_offset + self.alignment > self.buffer_size {
+            panic!("Buffer Overflow");
+        }
+
+        let offset = self.current_offset;
         queue.write_buffer(
-            &self.buffer,
-            0,
-            bytemuck::cast_slice(&[TransformUniform::new(matrix, projection)]),
-        )
+            &self.buffers[self.current_buffer],
+            offset as u64,
+            bytemuck::cast_slice(&[*uniform]),
+        );
+
+        let result = (self.current_buffer, offset as u32);
+        self.current_offset += self.alignment;
+        result
+    }
+
+    pub fn next_frame(&mut self) {
+        self.current_buffer = (self.current_buffer + 1) % self.buffers.len();
+        self.current_offset = 0;
     }
 }
